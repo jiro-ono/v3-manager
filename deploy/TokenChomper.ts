bb490b3412ab35b70477299f5d7ff19942f736c6
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-05-01 00:32:38 +0700

    feat: deploy TokenChwomper

--- deploy/TokenChomper.ts
+++ deploy/TokenChomper.ts
@@ -1,11 +1,12 @@
 import type { HardhatRuntimeEnvironment } from "hardhat/types";
 import type { DeployFunction } from "hardhat-deploy/types";
-import { SKL, WNATIVE_ADDRESS } from "sushi/currency";
+import { WNATIVE_ADDRESS } from "sushi/currency";
 import {
 	isRouteProcessor6ChainId,
 	ROUTE_PROCESSOR_6_ADDRESS,
 } from "sushi/config";
 import { EvmChainId } from "sushi/chain";
+import { zeroAddress } from "viem";
 
 const getOperatorAddress = (_chainId: EvmChainId) =>
 	process.env.OPERATOR_ADDRESS;
@@ -15,7 +16,7 @@ const getRPAddress = (chainId: EvmChainId) =>
 		? ROUTE_PROCESSOR_6_ADDRESS[chainId]
 		: undefined;
 const getTokenAddress = (chainId: EvmChainId) =>
-	chainId === EvmChainId.SKALE_EUROPA ? SKL : WNATIVE_ADDRESS[chainId];
+	chainId === EvmChainId.SKALE_EUROPA ? zeroAddress : WNATIVE_ADDRESS[chainId];
 
 const func: DeployFunction = async ({
 	ethers,
